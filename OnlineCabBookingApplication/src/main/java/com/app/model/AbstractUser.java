17aaf7f4ff2086eb7fbfdc24651bee672da26cc8
Chetan0777 <coderchetan1@gmail.com>
2022-11-11 00:12:34 +0530

    admin controller and service

---
+++ OnlineCabBookingApplication/src/main/java/com/app/model/AbstractUser.java
@@ -0,0 +1,47 @@
+package com.app.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED)
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class AbstractUser {
+
+	
+	@Size(min = 5, max = 50, message = "{usernameRange}")
+	@NotNull(message = "{notNull}")
+	@Column(unique = true)
+	private String username;
+	
+	@Size(min = 6, max = 20, message = "{PasswordRange}")
+	@NotNull(message = "{notNull}")
+	private String password;
+	
+
+	private String address;
+	
+	@Size(max = 10, message = "{MobileRange}")
+	@Pattern(regexp = "[789]{1}[0-9]{9}", message = "{MobileRange}")
+	private String mobileNumber;
+	
+	@Email
+	@NotNull(message = "{notNull}")
+	private String email;
+	
+	
+}
