6df7e2e3d4a3c1dfeb10e6b84b2fbc833f960021
dambale123 <shubhamdambale123@gmail.com>
2022-11-10 11:22:05 +0530

    controller added

--- OnlineCabBookingApplication/src/main/java/com/app/controller/DriverController.java
+++ OnlineCabBookingApplication/src/main/java/com/app/controller/DriverController.java
@@ -2,4 +2,5 @@ package com.app.controller;
 
 public class DriverController {
 
+	
 }
---
+++ OnlineCabBookingApplication/src/main/java/com/app/repository/CabRepository.java
@@ -0,0 +1,19 @@
+package com.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.app.model.Cab;
+
+@Repository
+public interface CabRepository extends JpaRepository<Cab, Integer>{
+
+//	@Query("from Cab c where c.cabType = ?1")
+	public List<Cab> findByCarType(String cartype);
+	
+	
+	
+}
