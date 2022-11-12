59deb7205f121da5e62f38182fb05798f23ac675
Chetan0777 <coderchetan1@gmail.com>
2022-11-12 23:34:36 +0530

    admin controller

--- OnlineCabBookingApplication/src/main/java/com/app/controller/AdminController.java
+++ OnlineCabBookingApplication/src/main/java/com/app/controller/AdminController.java
@@ -1,6 +1,5 @@
 package com.app.controller;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,15 +7,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.dto.AdminDTO;
+import com.app.dto.Login;
 import com.app.exception.AdminException;
 import com.app.model.Admin;
-import com.app.model.TripBooking;
+import com.app.model.AdminSession;
+import com.app.model.Customer;
+import com.app.model.Driver;
 import com.app.service.AdminService;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -28,11 +33,19 @@ public class AdminController {
 	@Autowired
 	private AdminService adminService;
 	
+	@Autowired
+	private LoginService loginService;
+	
 	
 	@PostMapping("/insertadmin")
 	public ResponseEntity<Admin> insertAdminHandler(@RequestBody Admin admin) throws AdminException {
 		Admin savedAdmin = adminService.insertAdmin(admin);
 		return new ResponseEntity<Admin>(savedAdmin,HttpStatus.OK);
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<AdminSession> loginAdmin(@RequestBody Login dto) {
+		return new ResponseEntity<>(loginService.loginAdmin(dto), HttpStatus.ACCEPTED);
 	}
 	
 	@PutMapping("/updateadmin")
@@ -42,43 +55,52 @@ public class AdminController {
 
 	}
 	
+	@PatchMapping("/update/{username}")
+	public Admin updateAdminPassword(@RequestBody AdminDTO dto, @PathVariable("username") String username,
+			@RequestParam String key) {
+		return adminService.updatePassword(dto, username, key);
+	}
+	
 	@DeleteMapping("/deleteadmin/{adminId}")
 	public ResponseEntity<Admin> deleteMappingHandler(@PathVariable("adminId") Integer adminId) throws AdminException {
 		Admin deleteAdmin = adminService.deleteAdmin(adminId);
 		return new ResponseEntity<Admin>(deleteAdmin,HttpStatus.OK);
 	}
 	
-	@GetMapping("/getalltrips/{customerId}")
-	public ResponseEntity<List<TripBooking>>  getAllTripsHandler(@PathVariable("customerId") Integer customerId) throws AdminException{
-		List<TripBooking> trips= adminService.getAllTrips(customerId);
-		return new ResponseEntity<List<TripBooking>>(trips,HttpStatus.OK);
-	}
-	
-	@GetMapping("/trips/cabwise")
-	public ResponseEntity<List<TripBooking>> getTripsCabwiseHandler(){
-		List<TripBooking> cabwisetrips= adminService.getTripsCabwise();
-		return new ResponseEntity<List<TripBooking>>(cabwisetrips,HttpStatus.OK);
-	}
-	
-	@GetMapping("/customerwisetrips")
-	public List<TripBooking> getTripsCustomerwiseHandler(){
-		List<TripBooking> customerwiselist = adminService.getTripsCustomerwise();
-		return customerwiselist;
-	}
-	
-	@GetMapping("/datewisetrips")
-	public List<TripBooking> getTripsDatewiseHandler() throws AdminException{
-		List<TripBooking> datewiselist = adminService.getTripsDatewise();
-		return datewiselist;
-	}
-	
-	@GetMapping("trips/{customerId}/{fromDate}/{toDate}")
-	public List<TripBooking> getTripsDatewiseAndCustomerHandler(@PathVariable("customerId") Integer customerId, @PathVariable("fromDate") String fromDate,@PathVariable("toDate") String toDate) throws AdminException{
-		LocalDate fromDate1 = LocalDate.parse(fromDate);
-		LocalDate toDate1 = LocalDate.parse(toDate);
-		List<TripBooking> list = adminService.getAllTripsForDays(customerId, fromDate1, toDate1);
-		return list;
+	@GetMapping("/logout")
+	public String logoutAdmin(@RequestParam String key) {
+
+		return adminService.logoutAdmin(key);
 
 	}
+	
+	@GetMapping("/listoftripsbycustomer")
+	public List<CompletedTrips> listOfAllTripsByCustomer(@RequestParam Integer customerId, @RequestParam String key) {
+		return adminService.getTripsByCustomerId(customerId, key);
+
+	}
+
+	@GetMapping("/listoftrips")
+	public List<CompletedTrips> listOfAllTrips(@RequestParam String key) {
+		return adminService.getAllTrips(key);
+	}
+
+	@GetMapping("/listofcustomers")
+	public List<Customer> listOfCustomers(@RequestParam String key) {
+
+		List<Customer> listOfCustomers = adminService.getListOfCustomers(key);
+
+		return listOfCustomers;
+	}
+
+	@GetMapping("/listofdrivers")
+	public List<Driver> listOfDrivers(@RequestParam String key) {
+
+		List<Driver> listOfDrivers = adminService.getListOfDrivers(key);
+
+		return listOfDrivers;
+	}
+	
+	
 	
 }
---
+++ OnlineCabBookingApplication/src/main/java/com/app/exception/LoginException.java
@@ -0,0 +1,15 @@
+package com.app.exception;
+
+public class LoginException extends RuntimeException {
+
+	public LoginException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginException(String sms) {
+
+		super(sms);
+		// TODO Auto-generated constructor stub
+	}
+
+}
